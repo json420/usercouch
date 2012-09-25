70798a2df8dfbd5f9607c751ffed2499f697fbd9
Jason Gerard DeRose <jderose@novacut.com>
2012-09-25 00:17:22 -0600

    Oops, can't actually use -sha384 in real life yet :(

--- usercouch/sslhelpers.py
+++ usercouch/sslhelpers.py
@@ -46,7 +46,7 @@ def gen_ca(key_file, subject, dst_file):
     check_call(['openssl', 'req',
         '-new',
         '-x509',
-        '-sha384',
+        #'-sha384',
         '-days', '3650',
         '-key', key_file,
         '-subj', subject,
@@ -62,7 +62,7 @@ def gen_csr(key_file, subject, dst_file):
     """
     check_call(['openssl', 'req',
         '-new',
-        '-sha384',
+        #'-sha384',
         '-key', key_file,
         '-subj', subject,
         '-out', dst_file,
@@ -75,7 +75,7 @@ def gen_cert(csr_file, ca_file, key_file, srl_file, dst_file):
     """
     check_call(['openssl', 'x509',
         '-req',
-        '-sha384',
+        #'-sha384',
         '-days', '3650',
         '-CAcreateserial',
         '-in', csr_file,
