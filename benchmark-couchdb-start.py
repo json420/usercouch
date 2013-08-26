48fad533668d7f6689312190a5b3b460c59b03e2
Jason Gerard DeRose <jason@system76.com>
2013-08-25 22:21:05 -0600

    Added a quick benchmark for CouchDB startup time

---
+++ benchmark-couchdb-start.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# usercouch: Start per-user CouchDB instances for fun and unit testing
+# Copyright (C) 2013 Novacut Inc
+#
+# This file is part of `usercouch`.
+#
+# `usercouch` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `usercouch` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with `usercouch`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+
+"""
+Benchmark time it takes CouchDB to start.
+"""
+
+import time
+
+from usercouch.misc import TempCouch
+
+
+count = 10
+times = []
+for i in range(count):
+    tmp = TempCouch()
+    start = time.monotonic()
+    tmp.bootstrap()
+    elapsed = time.monotonic() - start
+    times.append(elapsed)
+    
+print('')
+print('Average: {:.3f}'.format(sum(times) / count))
+print('Max: {:.3f}'.format(max(times)))
+print('Min: {:.3f}'.format(min(times)))
