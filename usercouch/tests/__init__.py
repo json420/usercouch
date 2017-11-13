4d99b642f70e0f3627632887fedbd3d06a4ea0c4
Jason Gerard DeRose <jason@system76.com>
2017-11-13 10:39:24 -0700

    Add 1.7.1 to version check list

--- usercouch/tests/__init__.py
+++ usercouch/tests/__init__.py
@@ -2186,7 +2186,7 @@ class TestUserCouch(TestCase):
             )
         )
         self.assertIn(uc._welcome['version'],
-            ('1.4.0', '1.5.0', '1.5.1', '1.6.0', '1.6.1', '1.7.0', '2.0.0', '2.1.0')
+            ('1.4.0', '1.5.0', '1.5.1', '1.6.0', '1.6.1', '1.7.0', '1.7.1', '2.0.0', '2.1.0')
         )
         if 'uuid' in uc._welcome:
             self.assertIsInstance(uc._welcome['uuid'], str)
